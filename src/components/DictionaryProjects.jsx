c6455667cc4074e13ddc7b6387780a4e397b9924
Carles <carlesmatosesg@gmail.com>
2024-01-03 23:34:01 +0100

    dist

---
+++ src/components/DictionaryProjects.jsx
@@ -0,0 +1,33 @@
+const dictionary_projects = [
+    {
+        nombre: "Color Management - Introducción| Ep1 | ES |",
+        description: `Me gustaría compartir algunos de los conocimientos que
+        he adquirido investigando la gestión de color. Creo que pueden ser 
+        muy útiles para elegir los métodos mas ajustados a los diferentes proyectos.
+        Se trata de un video un poco experimental. Todavía estoy buscando el 
+        formato más cómodo y fácil de entender.`,
+        image: "https://img.youtube.com/vi/xF_GKddmFJY/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=xF_GKddmFJY",
+    },
+    {
+        nombre: "Color Management - ¿Qué es OpenColorIO/OCIO? | Ep2 | ES |",
+        description: `Este es un pequeño resumen de como funcionan las 
+        configuraciones ocio. Presentan una gran versatilidad y las considero 
+        una herramienta muy útil. Es importante que los usuarios entiendan como 
+        funcionan para poder modificarlas y obtener la máxima customización para 
+        proyectos específicos.`,
+        image: "https://img.youtube.com/vi/3vSTqKgC5Z4/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=3vSTqKgC5Z4",
+    },
+    {
+        nombre: "Color Management - ¿Qué es Filmic Blender? | Ep3 | ES |",
+        description: `He decidido mostrar de forma gráfica el comportamiento 
+        de la configuración que emplea blender. 
+        Al mismo tiempo, también señalo algunas decisiones 
+        que me molestan un poco de la forma que incorpora blender 
+        la configuración en su software.`,
+        image: "https://img.youtube.com/vi/iIL-srRsbbU/hqdefault.jpg",
+        url: "https://www.youtube.com/watch?v=iIL-srRsbbU",
+    },
+];
+export default  dictionary_projects
--- src/pagina/proyectos.jsx
+++ src/pagina/proyectos.jsx
@@ -1,47 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { Col, Container, Image, Row } from "react-bootstrap";
-
-
-const diccionario_proyectos = [
-    {
-        nombre: "Color Management - Introducción| Ep1 | ES |",
-        description: `Me gustaría compartir algunos de los conocimientos que
-        he adquirido investigando la gestión de color. Creo que pueden ser 
-        muy útiles para elegir los métodos mas ajustados a los diferentes proyectos.
-        Se trata de un video un poco experimental. Todavía estoy buscando el 
-        formato más cómodo y fácil de entender.`,
-        image: "https://img.youtube.com/vi/xF_GKddmFJY/hqdefault.jpg",
-        url: "https://www.youtube.com/watch?v=xF_GKddmFJY",
-    },
-    {
-        nombre: "Color Management - ¿Qué es OpenColorIO/OCIO? | Ep2 | ES |",
-        description: `Este es un pequeño resumen de como funcionan las 
-        configuraciones ocio. Presentan una gran versatilidad y las considero 
-        una herramienta muy útil. Es importante que los usuarios entiendan como 
-        funcionan para poder modificarlas y obtener la máxima customización para 
-        proyectos específicos.`,
-        image: "https://img.youtube.com/vi/3vSTqKgC5Z4/hqdefault.jpg",
-        url: "https://www.youtube.com/watch?v=3vSTqKgC5Z4",
-    },
-    {
-        nombre: "Color Management - ¿Qué es Filmic Blender? | Ep3 | ES |",
-        description: `He decidido mostrar de forma gráfica el comportamiento 
-        de la configuración que emplea blender. 
-        Al mismo tiempo, también señalo algunas decisiones 
-        que me molestan un poco de la forma que incorpora blender 
-        la configuración en su software.`,
-        image: "https://img.youtube.com/vi/iIL-srRsbbU/hqdefault.jpg",
-        url: "https://www.youtube.com/watch?v=iIL-srRsbbU",
-    },
-];
-
+import DictionaryProjects from "../components/DictionaryProjects";
 
 function Proyecto_plantilla({ proyectos }) {
     return (
         <Container className="pt-4">
             {proyectos.map((proyecto, index) => (
-
                 <Row className="pt-4 justify-content-center " key={index}>
                     <Col bsPrefix="col-auto my-auto" style={{ width: "300px" }}>
                         <Link to={`${proyecto.url}`}>
@@ -61,14 +26,11 @@ function Proyecto_plantilla({ proyectos }) {
     );
 }
 
-
 function Proyectos() {
-
-
     return (
 
         <Container>
-            <Proyecto_plantilla proyectos={diccionario_proyectos} />
+            <Proyecto_plantilla proyectos={DictionaryProjects} />
         </Container>
     )
 }
